69aedae936c311cf1a671a760bb15d82c8ed3cce
NKOSI_DANDORA <222939904+nkosinathi2013@users.noreply.github.com>
2026-06-26 12:51:21 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,11 +6,11 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/mruniquehacker/Knightbot-md');
+    const res = await fetch('https://github.com/nkosinathi2013/DANDORA-MD/tree/main');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*乂  Knight Bot MD  乂*\n\n`;
+    let txt = `*乂  𝙳𝙰𝙽𝙳𝙾𝚁𝙰-𝙼𝙳  乂*\n\n`;
     txt += `✩  *Name* : ${json.name}\n`;
     txt += `✩  *Watchers* : ${json.watchers_count}\n`;
     txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
@@ -18,7 +18,7 @@ async function githubCommand(sock, chatId, message) {
     txt += `✩  *URL* : ${json.html_url}\n`;
     txt += `✩  *Forks* : ${json.forks_count}\n`;
     txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
-    txt += `💥 *KnightBot MD*`;
+    txt += `💥 *𝙳𝙰𝙽𝙳𝙾𝚁𝙰-𝙼𝙳*`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/bot_image.jpg');
